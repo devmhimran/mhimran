ad3b144669692f5c59a3f47ca9fba7cdc2230e3a
Mahmud Hasan Imran <mahmud.bubt.150@gmail.com>
2022-08-12 20:04:19 +0600

    Update Hero & Navbar, Added MySkill & About

--- mhimran/src/Component/Hero/Hero.js
+++ mhimran/src/Component/Hero/Hero.js
@@ -4,19 +4,19 @@ import heroImage from '../Assets/hero-image.png'
 
 const Hero = () => {
     return (
-        <div className='container mx-auto py-24'>
-            <div class="grid grid-cols-2 gap-4">
+        <div className='container mx-auto lg:py-24 md:py-16 sm:py-16'>
+            <div class="grid lg:grid-cols-2 md:grid-cols-2 sm:grid-cols-2 gap-4">
                 <div className="hero__left flex items-center">
                     <div className="hero__left__main">
-                        <h3 className='text-green-500 text-2xl'>Hi, I am</h3>
+                        <h3 className='text-lime-500 text-2xl'>Hi, I am</h3>
                         <h1 className='text-white text-7xl font-bold'>Mahmud<br></br> Hasan Imran</h1>
                         <div className="details mt-3">
-                            <h3 className='text-2xl font-semibold text-green-500'>Front End Developer</h3>
+                            <h3 className='text-2xl font-semibold text-lime-500'>Front End Developer</h3>
                             <div className='text-white text-lg'>
                                 <p className='inline'>Web Developer</p>
-                                <GoPrimitiveDot className='inline mx-3 text-green-400'></GoPrimitiveDot>
+                                <GoPrimitiveDot className='inline mx-3 text-lime-500'></GoPrimitiveDot>
                                 <p className='text-white inline'>WordPress Expert</p>
-                                <GoPrimitiveDot className='inline mx-3 text-green-400'></GoPrimitiveDot>
+                                <GoPrimitiveDot className='inline mx-3 text-lime-500'></GoPrimitiveDot>
                                 <p className='text-white inline'>Computer Engineer</p>
                             </div>
                         </div>
--- mhimran/src/Component/Navbar/Navbar.js
+++ mhimran/src/Component/Navbar/Navbar.js
@@ -9,27 +9,27 @@ const Navbar = () => {
         <li><Link to='#about'>About</Link></li>
         <li><Link to='#portfolio'>Portfolio</Link></li>
         <li><Link to='#blog'>Blog</Link></li>
+        <button className='px-6 border-2 border-lime-500 rounded-md'>Resume</button>
     </>
     return (
-        <div class="navbar text-white py-3">
+        <div class="navbar text-white py-8">
             <div class="navbar-start">
-                <div class="dropdown">
+            <div class="dropdown">
                     <label tabIndex="0" class="btn btn-ghost lg:hidden">
                         <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </label>
-                    <ul tabIndex="0" class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                    <ul tabIndex="0" class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52 text-black">
                         {navLink}
                     </ul>
                 </div>
-                
                 <Link to='/'><img className='w-8/12' src={devmhimranLogo} alt="devmhimranLogo" /></Link>
+                
             </div>
             <div class="navbar-end hidden text-lg lg:flex">
                 <ul class="menu menu-horizontal p-0">
                     {navLink}
-                </ul>
+                </ul>              
             </div>
-            
         </div>
     );
 };
