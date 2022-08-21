0e1d4157aad4cfcce8213c3ad128c6156a073547
Mahmud Hasan Imran <mahmud.bubt.150@gmail.com>
2022-08-21 19:17:22 +0600

    Update Portfolio Section

--- mhimran/src/Component/Portfolio/Portfolio.js
+++ mhimran/src/Component/Portfolio/Portfolio.js
@@ -15,15 +15,16 @@ import { AiFillGithub } from 'react-icons/ai';
 const Portfolio = () => {
 
     return (
-        <div className='portfolio' id='portfolio'>
+        <div className='portfolio bg-sky-700' id='portfolio'>
             <div className="container mx-auto">
                 <div className="portfolio__main max-w-screen-xl mx-auto lg:py-24 md:py-16 py-16">
                     <h1 className='text-3xl lg:text-5xl font-bold mb-12 lg:mb-16 text-center lg:text-left'>Portfolio</h1>
                     <div className="grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-4 justify-center">
-                        <Card className="w-96">
-                            <CardHeader className="h-80">
+                        <Card className="w-96 shadow-white border border-gray-200 rounded-lg">
+                            {/* <CardHeader className="h-80">
                                 <img src="https://devmhimran.webzoogoal.com/wp-content/uploads/2022/06/find-tools-img-1024x719.png" alt="" />
-                            </CardHeader>
+                            </CardHeader> */}
+                            <img  className='rounded-t-lg' src="https://devmhimran.webzoogoal.com/wp-content/uploads/2022/06/find-tools-img-1024x719.png" alt="" />
                             <CardBody className="text-center">
                                 <Typography variant="h4" color="blue-gray" className="mb-2">
                                     MoboPedia
