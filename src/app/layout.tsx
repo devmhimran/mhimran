d00ad05a02317e2de02bd3faa0d2150d87f59f7e
Imran <mahmud.bubt.150@gmail.com>
2025-03-17 03:42:37 +0600

    added tag manager

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next';
 import { Montserrat } from 'next/font/google';
 import './globals.css';
 import localFont from 'next/font/local';
-import { GoogleAnalytics } from '@next/third-parties/google';
+import { GoogleAnalytics, GoogleTagManager } from '@next/third-parties/google';
 
 import { cn } from './lib/utils';
 
@@ -64,6 +64,9 @@ export default function RootLayout({
       <head>
         <GoogleAnalytics
           gaId={process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID || ''}
+        />
+        <GoogleTagManager
+          gtmId={process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID || ''}
         />
       </head>
       <body
